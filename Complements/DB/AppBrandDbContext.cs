ab83d22e571c59fb6dd84cf9c5bac0c4ca182f93
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2026-07-10 11:21:07 -0600

    Add EF Core, DbContext, and BrandRepository integration
    Added Entity Framework Core dependencies and implemented AppBrandDbContext for BrandModel. Introduced BrandRepository with async CRUD operations for Brand entities. Updated WinformsCleanArchitecture to reference Complements project.

---
+++ Complements/DB/AppBrandDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Complements.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complements.DB;
+
+public class AppBrandDbContext : DbContext
+{
+    public AppBrandDbContext(DbContextOptions<AppBrandDbContext> options) : base(options) { }
+
+    public DbSet<BrandModel> Brands { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<BrandModel>().ToTable("Brand");
+    }
+}
---
+++ Complements/Repository/BrandRepository.cs
@@ -0,0 +1,52 @@
+﻿using Complements.DB;
+using Complements.Entities;
+using Complements.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complements.Repository;
+
+public class BrandRepository : IRepository<Brand>
+{
+    private readonly AppBrandDbContext _context;
+
+    public BrandRepository(AppBrandDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Brand entity)
+    {
+        var brandModel = new BrandModel()
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            Description = entity.Description
+        };
+
+        await _context.AddAsync(brandModel);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Brand>> GetAllAsync() => await _context.Brands.Select(brandEntity => new Brand
+    {
+        Id = brandEntity.Id,
+        Name = brandEntity.Name,
+        Description = brandEntity.Description
+    }).ToListAsync();
+
+    public async Task<Brand> GetByIdAsync(int id)
+    {
+        var brandModel = await _context.Brands.FindAsync(id);
+        if (brandModel == null) return null;
+
+        return new Brand
+        {
+            Id = brandModel.Id,
+            Name = brandModel.Name,
+            Description = brandModel.Description
+        };
+    }
+}
